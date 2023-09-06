a3792878c42526d64b683964eb34ac5a50b66300
Ramya.U <ramyayadav2016@gmail.com>
2023-09-06 12:13:30 +0530

    seed data migration

--- CodingWiki.Infrastructure/EF/Data/CodingWikiDBContext.cs
+++ CodingWiki.Infrastructure/EF/Data/CodingWikiDBContext.cs
@@ -26,6 +26,21 @@ namespace CodingWiki.Infrastructure.EF.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Suits", ISBN = "123B120", Price = 10.99m },
+                new Book { BookId = 2, Title = "Never Have I Ever", ISBN = "12123B120", Price = 11.99m }
+                );
+
+            var bookList = new Book[]
+            {
+                new Book{  BookId = 3, Title = "Vampire Diaries", ISBN = "10123B120", Price = 17.99m },
+                new Book{  BookId = 4, Title = "Red Hoodie", ISBN = "10188B170", Price = 87.99m },
+                new Book{  BookId = 5, Title = "Hilda", ISBN = "10180AB170", Price = 47.99m },
+            };
+
+            modelBuilder.Entity<Book>().HasData(bookList);
         }
     }
 }
